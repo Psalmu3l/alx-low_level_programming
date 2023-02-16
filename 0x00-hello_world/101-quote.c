dadaa1545fc210ccedfd67a0dea30dfcda5358a2
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-16 14:49:06 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
 /**
- * main - A program thatv prints a line with write function
- * Return: 0 (Success)
+ * Description: the main returns the value
  */
 int main(void)
 {
 	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
