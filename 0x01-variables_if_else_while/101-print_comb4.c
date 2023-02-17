b0b4df9d7dc0042f7e0c7e1bb4de5d15a60b4811
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-17 14:17:41 -0800

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Print 4 combinations
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ones = '0';
+
+	int tens = '0';
+
+	int hundreds = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens)))
+	/* eliminate repititon*/
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9'
+	&& hundreds == '7' && tens == '8'))/* adds commas and space*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
