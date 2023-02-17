d16f138e65bc1994bb0890614a300bdef1ad7a80
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-17 12:48:10 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print numbers in base 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
