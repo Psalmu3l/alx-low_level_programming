183368e290e62608f66d73aafc0112c62ee85603
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-23 15:04:57 -0800

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * print_diagonal - Draws a diagonal lines according parameter
  * @n: The number of times to print diagonal lines
  * Return: empty
  */
-void print_diagonal(int n);
+
+void print_diagonal(int n)
+
 {
 	int x, y;
 
