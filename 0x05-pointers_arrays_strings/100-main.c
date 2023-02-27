21f8a3569326fbe3716c09040c2895157c8afacf
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-27 12:51:39 -0800

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(boid)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi(("------++++++-----+++++--98");
+	printf"%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("       +       +       -       -      -98 Bttery Street; San Francisco, CA 94111 - USA        ");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - tr - 402 #cosfun :)");
+	printf("%d\n", nb);
+	return (0);
+}
