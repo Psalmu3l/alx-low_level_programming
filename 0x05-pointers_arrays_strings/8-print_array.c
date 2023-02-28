f30574bb43e9dc1ec722425881ed9763bd483063
psalmu3l <sammyp791.sp37@gmail.com>
2023-02-27 19:31:32 -0800

    commited

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,12 @@
 void print_array(int *a, int n)
 
 {
-	int j;
+	int j = 0;
 
 	for (j = 0; j < n; j++)
 	{
 		printf("%d", a[j]);
-		if (j != (n - 2))
+		if (j < n - 1)
 		{
 			printf(",");
 		}
