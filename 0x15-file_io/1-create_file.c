cdb9f236111ffeeda56dc853230476def43c0cd7
Psalmu3l <sammyp791.sp37@gmail.com>
2023-04-11 03:25:54 -0700

    repair

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,10 @@
 /**
   * create_file - A function that creates a file
   * with rw------- permissions
+  * @text_content: the text file content.
   * @filename: name of the file, if NULL, return   * -1
   * Return: 1 on success, -1 on failure
-  **/
+ **/
 
 int create_file(const char *filename, char *text_content)
 {
